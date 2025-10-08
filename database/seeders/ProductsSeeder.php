ea3934c9f1aece722632e2a63f8923be1d7e9b24
Sonu445Kum <sonuroy1629@gmail.com>
2025-10-08 10:56:34 +0530

    Create a ProductSeeder.php files for Insert the Data in the DB

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product; // If you are using Product model
+
+class ProductsSeeder extends Seeder
+{
+    public function run()
+    {
+        // your seeding logic
+    }
+}
